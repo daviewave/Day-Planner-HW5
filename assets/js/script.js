d613e6612e578569489b19ff7cfaa972fd7a7a4c
David Silveira <daviewave@gmail.com>
2021-10-24 18:32:37 -0400

    organized script.js so it is very easy to see how the code works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,44 @@
+//DEPENDENCIES
+//targeted html elements to edit
+var $dailyPlanner = $("#planner-section");
+var $currentDayHeader = $("#currentDay");
+
+//DATA
+//use moment.js to get the current day and hour
+const currentDay = moment().format("MMMM Do YYYY");
+var currentHour = moment().format("h");
+//if the user saves a task, will need an array to store it
+var savedTasks = [];
+
+//function called when page loads
 $(document).ready(function () {
-  //DEPENDENCIES
-  //DATA
-  //target html div id that will display daily planner rows and contents and set to empty
-  var $dailyPlanner = $("#planner-section");
-  $dailyPlanner.empty();
+  setTodaysDate();
+  clearPlanner();
+  buildDailyPlanner();
+});
 
-  //use moment.js to get the current day
-  const currentDay = moment().format("MMMM Do YYYY");
-
-  //target html p id that will display todays current date to the header and use variable holding date found with moment.js
-  var $currentDayHeader = $("#currentDay");
-  $currentDayHeader.text(currentDay);
-
-  //variable that stores the current hour (in 12 hour time format)
-  var currentHour = moment().format("h");
-  console.log(currentHour);
-
-  //if the user saves a task, will need an array to store it
-  var savedTasks = [];
-
-  //FUNCTIONS
-  /* PSUEDOCODE // 
+//FUNCTIONS
+/* PSUEDOCODE // 
 // 1. Build each row of the daily planner for each hour in the work day 9-5
     1.A. add a time for each row
     1.B. add a text-input ability in each row
     1.C. add a save input button in each row 
-*/
+*/ /* 2. Determinw what hour of the day it currently is 
+    2.A. if the current hour isnt between 9-5 make the hour row input color normal color
+    2.B. if the current hour is between 9-5 make that row color red
+    2.C. make all hours after current hour red
+    */
 
+//Helper functions
+function setTodaysDate() {
+  $currentDayHeader.text(currentDay);
+}
+
+function clearPlanner() {
+  $dailyPlanner.empty();
+}
+
+function buildDailyPlanner() {
   //since planner will only contain work-day hours, need 9 rows (9-5)
   for (var hourOnPlanner = 9; hourOnPlanner < 18; hourOnPlanner++) {
     //1.A. add 9 rows to the planner, 1 for each day
@@ -65,12 +77,4 @@ $(document).ready(function () {
     //will be the last step to add the row and all its new contents to the daily planner
     $dailyPlanner.append($rowContainer);
   }
-
-  /* 2. Determinw what hour of the day it currently is 
-    2.A. if the current hour isnt between 9-5 make the hour row input color normal color
-    2.B. if the current hour is between 9-5 make that row color red
-    2.C. make all hours after current hour red
-    */
-
-  //USER-INTERACTIONS
-});
+}
