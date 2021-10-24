32b50787f6a139871a1a15196491c03ef57e1510
David Silveira <daviewave@gmail.com>
2021-10-24 13:55:44 -0400

    added a target for the html element that will target where the planning will go

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,23 @@
-//DEPENDENCIES
+$(document).ready(function () {
+  //DEPENDENCIES
+  var $dailyPlanner = $("#planner-section"); //target html div that will display daily planner rows and contents
+  $dailyPlanner.empty();
 
-//DATA
+  //DATA
 
-//FUNCTIONS
-/* PSUEDOCODE // 
+  //FUNCTIONS
+  /* PSUEDOCODE // 
 // 1. Build each row of the daily planner for each hour in the work day 9-5
     1.A. add a time for each row
     1.B. add a text-input ability in each row
     1.C. add a save input button in each row 
+*/
 
-// 2. Determinw what hour of the day it currently is 
+  /* 2. Determinw what hour of the day it currently is 
     2.A. if the current hour isnt between 9-5 make the hour row input color normal color
     2.B. if the current hour is between 9-5 make that row color red
     2.C. make all hours after current hour red
- 
+    */
 
-//USER INTERACTIONS
+  //USER-INTERACTIONS
+});
